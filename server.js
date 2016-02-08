05b24e4822b0ee6f135a3ca8249a6175ee7a7e14
LandonPowell <landonjpowell@airmail.cc>
2016-02-08 03:29:14 +0000

    CSS, Index.html, other shit

--- server.js
+++ server.js
@@ -7,10 +7,12 @@
 var io = require('socket.io')();
 var express = require('express');
 var multer = require('multer');
-var settings = require('settings.json');
+var jsonfile = require('jsonfile');
+var settings = require('./settings.json');
 var app = express();
 
 // Global Variables
+var database = {};
 var ImageCounter = {};
 var tempImage = 0;
 
@@ -26,6 +28,14 @@ function htmlEscape(userPost) {
                   .replace(/\n/g, "<br/>");
 }
 
+// Setup Database
+jsonfile.readFile('database.json', function(err, obj) {
+    database = obj;
+    if (err) {
+        console.log('Database errors: '+err);
+    }
+});
+
 // Config Multer
 var storage = multer.diskStorage({
    destination: function (req, file, callback) {
@@ -33,12 +43,55 @@ var storage = multer.diskStorage({
    },
    filename: function (req, file, callback) {
       tempImage++;
-      var ext = file.originalname.split(".")[this.length-1];
-      if (settings.filetypes.indexOf(ext) > -1) {
+      var ext = file.originalname.split(".");
+      ext = ext[ext.length-1];
+      if (settings.filetypes.indexOf(ext.toLowerCase()) > -1) {
          callback(null, tempImage+'.'+ext);
-      } else {
+      } 
+      else {
          return false;
       }
    }
 });
+var upload = multer({ 
+    storage: storage,
+    onFileUploadStart: function(file, req, res){
+        res.redirect(req.files.file.path);
+    }
+});
 
+// File upload
+app.use('/', express.static(__dirname + '/public/'));
+
+app.post('/upload', upload.single('file'), function(req, res){
+   res.redirect(req.file.filename);
+   res.status(204).end();
+});
+
+
+app.get('/rand', function(req, res) {
+   var board = req.param('board');
+   var thread = req.param('thread');
+   //var post = req.param('post');
+   var pID = parseInt(thread, 36)-1;
+   if (thread) {
+      if (pID < database[board].threads.length) {
+         res.send(database[board].threads[pID]);
+      } else {
+         res.send("No thread.");
+      }
+   } else {
+      var send = "<script>var threadTitles = [";
+      for (var i = 0; i < database[board].threads.length; i++) {
+         send += '"'+database[board].threads[i].title+'"';
+         if (i < database[board].threads.length-1) {
+            send += ", ";
+         }
+      }
+      send += "]</script>";
+      res.send(send);
+   }
+});
+
+// Start server
+app.listen(process.env.PORT);
