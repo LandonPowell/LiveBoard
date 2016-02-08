b398bc7815b7d229bf91e266478c9edf6f15d4fb
LandonPowell <landonjpowell@airmail.cc>
2016-02-08 00:01:19 +0000

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # LiveBoard
 A live imageboard made by KitsumiTheFox (Jayce Webb) and Xultra (Landon Powell)
+
+### Depends Upon
+* Node.JS
+* Socket.io
+* Express
+* Multer
+
+### Developer Notes
+
+Indentation should be done with LJPowell indents, using 3-spaces, like so:
+```
+if (x) {
+   console.log("Ja!");
+}
+else {
+   console.log("Nein!");
+}
+```
+
+Whenver possible, fuctions should be defined like so:
+```
+function funcName(args) {
+   return args + 1;
+}
+```
+
+### To Run
+* First, change directory to the location of your SeaFour folder. For me, this would be
+
+
+   `cd ~/Prog/WebDev/LiveBoard/`
+
+
+* Next, npm install the dependencies in `package.json`.
+
+
+   `npm install`
+  
+
+* Finally, run node's server.js file.  
+
+
+   `node server.js`
+
+
+* You're now running LiveBoard. 
+
+Copyright (c) 2015 Jayce Webb and Landon Powell
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 /*********************
  *     LiveBoard     *
  * A live imageboard *
- *                   *
  *********************/
+ 
 // Node Modules
 var io = require('socket.io')();
 var express = require('express');
 var multer = require('multer');
 var app = express();
+
+// Global Variables
+var ImageCounter = {};
+var tempImage = 0;
 
 // Global Functions
 function htmlEscape(userPost) {
@@ -23,13 +27,14 @@ function htmlEscape(userPost) {
 
 // Config Multer
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/uploads/')
-    },
-    filename: function (req, file, cb) {
-        var ext = file.originalname.split(".");
-        ext = ext[ext.length-1];
-        cb(null, name+'.'+ext)
-    }
+   destination: function (req, file, cb) {
+      cb(null, 'temp/');
+   },
+   filename: function (req, file, cb) {
+      tempImage++;
+      var ext = file.originalname.split(".");
+      ext = ext[ext.length-1];
+      cb(null, tempImage+'.'+ext);
+   }
 });
 
